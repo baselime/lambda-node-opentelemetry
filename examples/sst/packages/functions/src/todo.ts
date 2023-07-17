b8d790b2b1bbe4788c7999c99f1fdecb22363160
Ankcorn <thomasankcorn@gmail.com>
2023-07-17 16:45:23 +0100

    add a callback example

--- examples/sst/packages/functions/src/todo.ts
+++ examples/sst/packages/functions/src/todo.ts
@@ -1,8 +1,10 @@
-export const handler = async () => {
-  console.log("TODO LIST", JSON.stringify({ MESSAGE:[{ id: 1, text: "TODO 1" }]}));
-  
-  return {
+import { APIGatewayProxyEventV2, Callback, Context } from "aws-lambda";
+
+export const handler = (a: APIGatewayProxyEventV2, b: Context, c: Callback) => {
+  console.log("TODO LIST", JSON.stringify({ MESSAGE: [{ id: 1, text: "TODO 1" }] }));
+
+  c(null, {
     statusCode: 200,
-    body: JSON.stringify({ MESSAGE:[{ id: 1, text: "TODO 12" }]}),
-  }
+    body: JSON.stringify({ MESSAGE: [{ id: 1, text: "TODO 12" }] }),
+  })
 }
