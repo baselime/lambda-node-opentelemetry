bdb008b03910af2f69012715d091dec1a2a71e4a
Ankcorn <thomasankcorn@gmail.com>
2023-09-08 12:18:51 +0100

    add better loader diagnostics

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,12 +5,14 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [0.2.1] 2023-08-08
+
+- Add better loading diagnostics
+- 
 ## [0.2.0] 2023-08-07
 
 - fix importing of cjs from esm loader
   
- 
- The latest layer is: `arn:aws:lambda:${your-region-here}:097948374213:layer:baselime-node:19`
 
 ## [0.1.19] 2023-08-06
 
--- src/loader.ts
+++ src/loader.ts
@@ -1,19 +1,25 @@
 import path from "path";
 
-async function _tryImport(path:string) {
+let diagnostics: Error[] = []
+
+async function _tryImport(path: string): Promise<false | Record<string, unknown>>{
     try {
         return await import(path);
-    } catch(err) {
-        console.error(err)
-        return false;
+    } catch (err) {
+        if (err instanceof Error) {
+            diagnostics.push(err)
+        }
+        return false
     }
 }
 
 function _tryRequire(path: string) {
     try {
         return require(path);
-    } catch(err) {
-        console.error(err)
+    } catch (err) {
+        if (err instanceof Error) {
+            diagnostics.push(err)
+        }
         return false;
     }
 }
@@ -26,11 +32,14 @@ export async function load(taskRoot: string, originalHandler: string) {
     const functionName = pathDetails.ext.slice(1);
 
     const functionPath = path.resolve(taskRoot, pathDetails.dir, pathDetails.name);
-    
-    const lambda = await _tryImport(functionPath + '.js') || await _tryImport(functionPath + '.mjs') || _tryRequire(functionPath + '.js') || _tryRequire(functionPath + '.cjs')
 
-    if (!lambda) {
-        throw Error(`Could not load ${functionPath}.js or ${functionPath}.mjs`);
+    const lambda = await _tryImport(functionPath + '.js') || await _tryImport(functionPath + '.mjs');
+
+    if (lambda === false) {
+        if (process.env.BASELIME_DEBUG && diagnostics.length > 0) {
+            process.stdout.write(`Diagnostics for ${originalHandler}\n${diagnostics.map(d => JSON.stringify({ name: d.name, message: d.message, stack: d.stack })).join('\n')}\n`)
+        }
+        throw Error(`Could not load ${originalHandler}`);
     }
     return lambda[functionName];
 }
@@ -44,11 +53,14 @@ export function loadSync(taskRoot: string, originalHandler: string) {
     const functionName = pathDetails.ext.slice(1);
 
     const functionPath = path.resolve(taskRoot, pathDetails.dir, pathDetails.name);
-    
-    const lambda =_tryRequire(functionPath + '.js') || _tryRequire(functionPath + '.cjs')
+
+    const lambda = _tryRequire(functionPath + '.js') || _tryRequire(functionPath + '.cjs')
 
     if (!lambda) {
-        throw Error(`Could not load ${functionPath}.js or ${functionPath}.cjs`);
+        if (process.env.BASELIME_DEBUG && diagnostics.length > 0) {
+            process.stdout.write(`Diagnostics for ${originalHandler}\n${diagnostics.map(d => JSON.stringify({ name: d.name, message: d.message, stack: d.stack })).join('\n')}\n`)
+        }
+        throw Error(`Could not load ${originalHandler}`);
     }
     return lambda[functionName];
 }   
