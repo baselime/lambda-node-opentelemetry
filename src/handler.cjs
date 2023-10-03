f61fa7b9a68bf49a089ede59dac52cd5e486bbdb
Ankcorn <thomasankcorn@gmail.com>
2023-10-03 11:04:08 +0100

    fix build

---
+++ src/handler.cjs
@@ -0,0 +1,17 @@
+const { loadSync } = require('./load-sync');
+const { wrap } = require('./index');
+
+const actualHandler = process.env.BASELIME_ACTUAL_HANDLER;
+const taskRoot = process.env.LAMBDA_TASK_ROOT;
+
+if(!taskRoot) {
+    throw Error('LAMBDA_TASK_ROOT is not defined');
+}
+
+if(!actualHandler) {
+    throw Error('BASELIME_ACTUAL_HANDLER is not defined');
+}
+
+const handler = loadSync(taskRoot, actualHandler);
+
+exports.handler = wrap(handler);
---
+++ src/handler.mjs
@@ -0,0 +1,16 @@
+import { wrap } from './index';
+import { load } from './load-async'
+
+
+const actualHandler = process.env.BASELIME_ACTUAL_HANDLER;
+const taskRoot = process.env.LAMBDA_TASK_ROOT;
+
+if(typeof taskRoot !== 'string') {
+    throw Error('LAMBDA_TASK_ROOT is not defined');
+}
+
+if(typeof actualHandler !== 'string') {
+    throw Error('BASELIME_ACTUAL_HANDLER is not defined');
+}
+
+export const handler = wrap(await load(taskRoot, actualHandler));
